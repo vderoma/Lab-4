481e4d0de873e578349a509180c849d2efff4808
Vincent DeRoma <vinderoma97@gmail.com>
2018-02-14 17:11:51 -0500

    begin

---
+++ Lab 4/Controllers/Lab4Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_4.Controllers
+{
+    public class Lab4Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
