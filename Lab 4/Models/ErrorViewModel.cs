93b7fdd651a653da989ad444d1522e8389db0b38
Vincent DeRoma <vinderoma97@gmail.com>
2018-02-14 17:00:16 -0500

    Add project files.

---
+++ Lab 4/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab_4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
